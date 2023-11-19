ef30b123f4104d7b3194f8193b06d673e665e78c
gemmay45 <149249215+gemmay45@users.noreply.github.com>
2023-11-19 15:22:01 +0800

    Add files via upload

---
+++ plugin-sources/translate/src/index.js
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2007-2022 Crafter Software Corporation. All Rights Reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as published by
+ * the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
+/*
+ * Copyright (C) 2007-2022 Crafter Software Corporation. All Rights Reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as published by
+ * the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+import App from './App';
+
+const plugin = {
+  id: 'org.craftercms.plugin',
+  name: 'Translate Plugin',
+  description: 'A plugin to do translations',
+  author: 'CrafterCMS',
+  logo: null,
+  locales: {},
+  apps: [],
+  widgets: {
+    'org.craftercms.translatePlugin.components.reactComponent': App,
+  },
+  scripts: [],
+  stylesheets: [],
+  themes: []
+};
+
+export { App };
+
+export default plugin;
