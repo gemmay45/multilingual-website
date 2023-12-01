f1a9cbab726fdf25813a6e5e3c53169409d15ed8
admin admin <evaladmin@example.com>
2023-12-01 16:27:49 +0800

    "User admin wrote content /scripts/rest/search.get.groovy"

--- scripts/rest/search.get.groovy
+++ scripts/rest/search.get.groovy
@@ -21,7 +21,7 @@ def categories = params["categories[]"]
 def start = params.start ? params.start as Integer : 0
 def rows = params.rows ? params.rows as Integer : 10
 def siteLocale = request.getRequestURI().substring(1,3)
-def searchHelper = new SearchHelper(elasticsearchClient, urlTransformationService, siteLocale)
+def searchHelper = new SearchHelper(elasticsearchClient, urlTransformationService, "en")
 def results = searchHelper.search(userTerm, categories, start, rows)
 
 return results;
