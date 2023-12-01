4bcc49c29bdcaad6d3a861c24ffc8739df83f943
admin admin <evaladmin@example.com>
2023-12-01 16:51:33 +0800

    "User admin wrote content /scripts/rest/search.get.groovy"

--- scripts/rest/search.get.groovy
+++ scripts/rest/search.get.groovy
@@ -17,7 +17,7 @@
 import org.craftercms.sites.editorial.SearchHelper
 
 def userTerm = params.userTerm
-def categories = categories['style','entertainment']
+def categories = params["categories[]"]
 def start = params.start ? params.start as Integer : 0
 def rows = params.rows ? params.rows as Integer : 10
 def siteLocale = request.getRequestURI().substring(1,3)
