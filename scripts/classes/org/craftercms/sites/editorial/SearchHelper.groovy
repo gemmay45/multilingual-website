dcecb56ba7b66319ea8f6306bff3f044855e5f35
admin admin <evaladmin@example.com>
2023-12-02 16:26:39 +0800

    "User admin wrote content /scripts/classes/org/craftercms/sites/editorial/SearchHelper.groovy"

--- scripts/classes/org/craftercms/sites/editorial/SearchHelper.groovy
+++ scripts/classes/org/craftercms/sites/editorial/SearchHelper.groovy
@@ -71,7 +71,12 @@ class SearchHelper {
 
     if (categories) {
       // Filter by categories
-      query.filter(getFieldQueryWithMultipleValues("categories_o.item.key", categories))
+      /*query.filter(getFieldQueryWithMultipleValues("categories_o.item.key", categories))*/
+      .query(q -> q
+        .bool(b -> {
+          b.filter(getFieldQueryWithMultipleValues("categories_o.item.key", categories))
+        })
+      )
     }
 
     if (userTerm) {
