2e9ee1c4e16abadc5013b15613325a8a7e569370
admin admin <evaladmin@example.com>
2023-12-05 16:57:51 +0800

    "User admin wrote content /scripts/classes/org/craftercms/sites/editorial/Utils.groovy"

--- scripts/classes/org/craftercms/sites/editorial/Utils.groovy
+++ scripts/classes/org/craftercms/sites/editorial/Utils.groovy
@@ -5,9 +5,28 @@ class Utils {
   private Utils() {
   }
 
-  /*
-  static def getAvailableItems(items) {
-
+def getAvailableItems(sourceItems) {
+        def count = 0
+        def availableItems
+        def items = []
+        
+        /*
+        def dirs = sourceItems
+            dirs.each { dir ->
+                def dirItem
+                
+                if (dir.component) {
+        			dirItem = dir.component
+                } else {
+        				dirItem = siteItemService.getSiteItem(dir.key.text)
+                }
+            
+                if (dirItem != null) {
+                    count=count+1
+                    items << dirItem
+                }
+            }
+        */
+        return count;
   }
-*/
 }
