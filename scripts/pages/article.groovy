4e62df96ba510bcce27d6d9de200b1e3407bc838
admin admin <evaladmin@example.com>
2023-12-05 12:48:39 +0800

    "User admin wrote content /scripts/pages/article.groovy"

--- scripts/pages/article.groovy
+++ scripts/pages/article.groovy
@@ -1,1 +1,17 @@
 import org.craftercms.sites.editorial.Utils
+
+
+def topNavItems = [:]
+def siteDir = siteItemService.getSiteTree("/site/website", 2)
+if(siteDir) {
+    def dirs = siteDir.childItems
+    dirs.each { dir ->
+            def dirName = dir.getStoreName()
+            def dirItem = siteItemService.getSiteItem("/site/website/${dirName}/index.xml")
+            if (dirItem != null) {
+                def dirDisplayName = dirItem.queryValue('internal-name')
+                   topNavItems.put(dirName, dirDisplayName)
+            }
+   }
+}
+model.topNavItems = topNavItems;
