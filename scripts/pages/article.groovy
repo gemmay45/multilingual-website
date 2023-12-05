159f6843d9dc91211fd4b67c4fb907e3e823c6e9
admin admin <evaladmin@example.com>
2023-12-05 14:50:47 +0800

    "User admin wrote content /scripts/pages/article.groovy"

--- scripts/pages/article.groovy
+++ scripts/pages/article.groovy
@@ -1,16 +1,19 @@
 import org.craftercms.sites.editorial.Utils
 
 def count = 0
-def availableItems = []
+def availableItems
+def item = []
 
     def dirs = contentModel.pageSections_o.item
     dirs.each { dir ->
         def dirItem = siteItemService.getSiteItem(dir.key.text)
         if (dirItem != null) {
             count=count+1
-            availableItems << dirItem
+            item << dirItem
         }
     }
+
+availableItems.item = item
 
 templateModel.count = count;
 templateModel.availableItems = availableItems;
