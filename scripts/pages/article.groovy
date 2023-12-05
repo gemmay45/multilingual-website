d6dd7545969a0ac696274c78067fe00ee6fe0646
admin admin <evaladmin@example.com>
2023-12-05 19:39:34 +0800

    "User admin wrote content /scripts/pages/article.groovy"

--- scripts/pages/article.groovy
+++ scripts/pages/article.groovy
@@ -26,7 +26,7 @@ templateModel.availableItems = items;
 */
 
 
-def getAvailableItems(sourceItems) {
+static def getAvailableItems(sourceItems) {
         def count = 0
         def availableItems
         def items = []
