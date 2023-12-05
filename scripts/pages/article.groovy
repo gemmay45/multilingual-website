f2a10fc4fec2ec8a8a42ac3cc52d4e68cad8c285
admin admin <evaladmin@example.com>
2023-12-05 14:12:25 +0800

    "User admin wrote content /scripts/pages/article.groovy"

--- scripts/pages/article.groovy
+++ scripts/pages/article.groovy
@@ -4,20 +4,20 @@ def count = 0
 def dirName = ""
 def topNavItems = [:]
 def availableItems = []
-/*def siteDir = siteItemService.getSiteTree("/site/components/en/the-mag/mag-with-shared-cmp", 1)*/
-if(siteDir) {
+/*def siteDir = siteItemService.getSiteTree("/site/components/en/the-mag/mag-with-shared-cmp", 1)
+if(siteDir) {*/
     def dirs = contentModel.pageSections_o.item
     dirs.each { dir ->
     count=count+1
             dirName = dir.getStoreName()
-            def dirItem = siteItemService.getSiteItem(dir.key)
+            def dirItem = siteItemService.getSiteItem(dir.key.text)
             if (dirItem != null) {
                 availableItems << dirItem
                 /*def dirDisplayName = dirItem.queryValue('internal-name')
                    topNavItems.put(dirName, dirDisplayName)*/
             }
    }
-}
+/*}*/
 templateModel.topNavItems = topNavItems;
 templateModel.count = count;
 templateModel.dirName = dirName;
