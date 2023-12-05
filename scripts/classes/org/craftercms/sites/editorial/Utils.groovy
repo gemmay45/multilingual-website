69181748297d8ecc1748fc0e9cad20f975ecc092
admin admin <evaladmin@example.com>
2023-12-05 20:31:30 +0800

    "User admin wrote content /scripts/classes/org/craftercms/sites/editorial/Utils.groovy"

--- scripts/classes/org/craftercms/sites/editorial/Utils.groovy
+++ scripts/classes/org/craftercms/sites/editorial/Utils.groovy
@@ -5,7 +5,7 @@ class Utils {
     private Utils() {
     }
 
-    static def getAvailableItems(sourceItems) {
+    static def getAvailableItems(sourceItems, siteItemService) {
         def count = 0
         def availableItems
         def items = []
