4d60c1cfbed3ecdd2b3db9878db50876bb58a104
admin admin <evaladmin@example.com>
2023-12-05 14:17:08 +0800

    "User admin wrote content /scripts/pages/article.groovy"

--- scripts/pages/article.groovy
+++ scripts/pages/article.groovy
@@ -9,7 +9,7 @@ if(siteDir) {*/
     def dirs = contentModel.pageSections_o.item
     dirs.each { dir ->
     count=count+1
-            dirName = dir.getStoreName()
+            /*dirName = dir.getStoreName()*/
             def dirItem = siteItemService.getSiteItem(dir.key.text)
             if (dirItem != null) {
                 availableItems << dirItem
@@ -20,5 +20,5 @@ if(siteDir) {*/
 /*}
 templateModel.topNavItems = topNavItems;*/
 templateModel.count = count;
-templateModel.dirName = dirName;
+/*templateModel.dirName = dirName;*/
 templateModel.availableItems = availableItems;
