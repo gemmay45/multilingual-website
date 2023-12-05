1ba25eb3798e16de4562812419ade7579cb08ac3
admin admin <evaladmin@example.com>
2023-12-05 15:49:07 +0800

    "User admin wrote content /scripts/pages/article.groovy"

--- scripts/pages/article.groovy
+++ scripts/pages/article.groovy
@@ -26,26 +26,6 @@ templateModel.availableItems = items;
 */
 
 def getAvailableItems(items) {
-        def count = 0
-        def availableItems
-        def items = []
-        
-            def dirs = items
-            dirs.each { dir ->
-                def dirItem
-                
-                if (dir.component) {
-        			dirItem = dir.component
-                } else {
-        				dirItem = siteItemService.getSiteItem(dir.key.text)
-                }
-            
-                if (dirItem != null) {
-                    count=count+1
-                    items << dirItem
-                }
-            }
-        
-        return count;
+
   }
 
