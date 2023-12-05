058510e099da2967945597de04cb635a98f3baba
admin admin <evaladmin@example.com>
2023-12-05 14:07:38 +0800

    "User admin wrote content /scripts/pages/article.groovy"

--- scripts/pages/article.groovy
+++ scripts/pages/article.groovy
@@ -3,6 +3,7 @@ import org.craftercms.sites.editorial.Utils
 def count = 0
 def dirName = ""
 def topNavItems = [:]
+def availableItems = []
 def siteDir = siteItemService.getSiteTree("/site/components/en/the-mag/mag-with-shared-cmp", 1)
 if(siteDir) {
     def dirs = siteDir.childItems
@@ -11,11 +12,13 @@ if(siteDir) {
             dirName = dir.getStoreName()
             def dirItem = siteItemService.getSiteItem("/site/components/en/the-mag/${dirName}/2f7299fc-d17d-4de7-b99b-e019a228d25f.xml")
             if (dirItem != null) {
-                def dirDisplayName = dirItem.queryValue('internal-name')
-                   topNavItems.put(dirName, dirDisplayName)
+                availableItems << dirItem
+                /*def dirDisplayName = dirItem.queryValue('internal-name')
+                   topNavItems.put(dirName, dirDisplayName)*/
             }
    }
 }
 templateModel.topNavItems = topNavItems;
 templateModel.count = count;
 templateModel.dirName = dirName;
+templateModel.availableItems = availableItems
