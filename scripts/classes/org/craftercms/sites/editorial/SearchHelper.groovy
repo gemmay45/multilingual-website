78aa7d9c51ff563140f77d13c4dcdd853427ea9e
admin admin <evaladmin@example.com>
2023-12-06 16:16:56 +0800

    "User admin wrote content /scripts/classes/org/craftercms/sites/editorial/SearchHelper.groovy"

--- scripts/classes/org/craftercms/sites/editorial/SearchHelper.groovy
+++ scripts/classes/org/craftercms/sites/editorial/SearchHelper.groovy
@@ -60,6 +60,14 @@ class SearchHelper {
         )
       )
     )
+    query.filter(q -> q
+      .match(m -> m
+          .field("localeCode_s")
+          .query(v -> v
+            .stringValue(this.locale)
+          )
+        )
+    )
 
     if (categories) {
       // Filter by categories
